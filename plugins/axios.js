2a6c40a3bffedcb51304fa7692f96cf59ab0766a
Félix Bouveret <bouveret.felix@gmail.com>
2020-05-01 15:41:43 +0200

    Fix: auto add token in header

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,10 @@
-export default function({ $axios, app, store }) {
+export default function({ $axios, app }) {
+  let auth = app.$cookies.get("auth");
+
+  if (auth) {
+    $axios.setToken(auth.token, "Bearer");
+  }
+
   $axios.onError(error => {
     // console.table(error);
     throw error.response;
